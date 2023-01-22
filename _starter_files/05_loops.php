cf80565aba3cc59a4f8fcf144283b7b3ac4884a3
KaitlinMelby <112581783+KaitlinMelby@users.noreply.github.com>
2023-01-22 15:32:12 -0800

    Loops update

--- _starter_files/05_loops.php
+++ _starter_files/05_loops.php
@@ -11,6 +11,11 @@
   }
 */
 
+//This will make the screen show 0-10 because it has x start at zero and increase by one untill it is les than or equal to 10
+for($x = 0; $x <= 10; $x ++) {
+  echo 'Number' .$x '<br>'; //this will add number before every single number
+};
+
 
 /* ------------ While Loop ------------ */
 
@@ -20,6 +25,13 @@
   // code to be executed
   }
 */
+
+$x =1;
+
+while($x <= 15) {
+  echo 'Number' . $x '<br>';
+  $x++ //without x++ it will just go until the browser crashes
+}
 
 
 /* ---------- Do While Loop --------- */
@@ -32,7 +44,12 @@
 
 do...while loop will always execute the block of code once, even if the condition is false.
 */
+$x = 6;
 
+do {
+  echo 'Number' . $x . '<br>';
+  $x++;
+} while($x <= 5); //This will show just the number six because it is executed before the do while loop
 
 
 /* ---------- Foreach Loop ---------- */
@@ -43,3 +60,17 @@ do...while loop will always execute the block of code once, even if the conditio
   // code to be executed
   }
 */
+
+$posts = ['First Post', 'Second Post', 'Third Post'];
+
+for($x = 0; $x < count($posts);$x++) {
+  echo $posts[$x];
+}; //will display all the posts in order
+
+foreach($posts as $post) {
+  echo $post;
+}; //Will give you the same thing as above 
+
+foreach($posts as $index=> $post) {
+  echo $index + 1 . '-' . $post . '<br>';
+}
