e25e39eb35287d2b9024b2e77436ee05614f97f4
KaitlinMelby <112581783+KaitlinMelby@users.noreply.github.com>
2023-01-22 15:15:03 -0800

    conditionals

--- _starter_files/04_conditionals.php
+++ _starter_files/04_conditionals.php
@@ -24,6 +24,37 @@ if (condition) {
 }
 */
 
+//if else statement
+
+$age = 20;
+
+if($age >= 18) {
+  echo 'You are old enough to vote';
+} else {
+eco 'Sorry you are not old enouth to vote';
+};
+
+//else if statements
+
+$t = date("H"); //H means hour
+
+if($t < 12) {
+  echo 'Good Morning';
+}  elseif($t < 17) {
+  echo 'Good Afternoon'
+}else {
+  echo 'good Evening';
+}
+
+$posts = ['First Post'];
+
+if(!empty ($posts)) {
+  echo $posts(0); 
+} else {
+  echo 'No Posts';
+};
+
+
 /* -------- Ternary Operator -------- */
 /*
   The ternary operator is a shorthand if statement.
@@ -33,3 +64,27 @@ if (condition) {
 
 
 /* -------- Switch Statements ------- */
+
+echo !empty($posts) ? $posts[0] : 'No Posts'; //does the same thing as the previous but in one line
+
+$firstPost = echo !empty($posts) ? $posts[0] : 'No Posts'; //can set these if else statements as variables
+
+$firstPost = $posts[0] ?? null;
+
+//switch (no not that kind)
+
+$favecolor = 'red';
+
+switch($favecolor) {
+  case 'red':
+    echo 'Your favorite color is red';
+    break
+    case 'blue':
+      echo 'Your favorite color is blue';
+      break
+      case 'green':
+        echo 'Your favorite color is green'
+        break;
+        default:
+        echo 'Your favorite color is not red, green, or blue';
+};
